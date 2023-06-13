2a0790b2b31e16f433ac4918b79f7df482d24261
astra-omapper <omapper926@gmail.com>
2023-06-13 12:00:32 +0300

    recoding javascript to bresenham

--- javascript/main.js
+++ javascript/main.js
@@ -42,53 +42,66 @@ class Field{
     }
     line(begin, end, char="#"){
         let lawyer = this.matrix;
-        // begin[0] -= 1;
-        // begin[1] -= 1;
-        // end[0] += 1;
-        // end[1] += 1;
         let deltaX = end[0] - begin[0];
         let deltaY = end[1] - begin[1];
         function buildByY(){
             let points = [];
-            let koef = deltaX/deltaY;
-            for(let coorY = begin[1]; coorY < end[1] + 1; coorY++){
-                let coorX = Math.round(coorY * koef) + begin[0];
-                if((coorX < lawyer.length) && (coorY < lawyer[0].length) && (deltaY != 0)){
-                    console.log("Y: ", [coorX, coorY])
-                    points.push([coorX, coorY]);
-                }else{
-                    console.log("IndexInfo: A line will builded by cycle via abscissa");
-                    buildByX();
-                    return
+            let koefX = 0;
+			if(end[0] < begin[0]){
+				koefX = -1;
+            }else{
+				koefX = 1;
+            };
+			let cornerKoef = (end[0]-begin[0])/(end[1]-begin[1])
+			let error = 0
+			let x = begin[0]
+			for(let y = begin[1]; y != end[1] + 1; y++){
+				if(error >= 0.5){
+					x += koefX;
+					error -= 1;
                 };
+				points.push([x, y]);
+				//console.log("x: ", x, " y: ", y, " e: ", error);
+				error += cornerKoef
             };
             for(let coors = 0; coors < points.length - 1; coors++){
-                console.log("build")
                 lawyer[points[coors][0]][points[coors][1]].char = char
             };
         };
         function buildByX(){
             let points = [];
-            let koef = deltaY/deltaX;
-            for(let coorX = begin[0]; coorX < end[0] + 1; coorX++){
-                let coorY = Math.round(coorX * koef) + begin[1];
-                if(coorX < lawyer.length && coorY < lawyer[0].length){
-                    console.log("X: ", [coorX, coorY])
-                    points.push([coorX, coorY]);
-                }else{
-                    console.log("IndexError: I can't build the line, because maybe coordinates are uncorrect");
+            let koefY = 0;
+			if(end[1] < begin[1]){
+				koefY = -1;
+            }else{
+				koefY = 1;
+            };
+			let cornerKoef = (end[1]-begin[1])/(end[0]-begin[0])
+			let error = 0
+			let y = begin[1]
+			for(let x = begin[0]; x != end[0] + 1; x++){
+				if(error >= 0.5){
+					y += koefY;
+					error -= 1;
                 };
-                for(let coors = 0; coors < points.length - 1; coors++){
-                    lawyer[points[coors][0]][points[coors][1]].char = char
-                };
+				points.push([x, y]);
+				//console.log("x: ", x, " y: ", y, " e: ", error);
+				error += cornerKoef
+            };
+            for(let coors = 0; coors < points.length - 1; coors++){
+                lawyer[points[coors][0]][points[coors][1]].char = char
             };
         };
-        buildByY();
+        if(Math.abs(begin[0] - end[0]) > Math.abs(begin[1] - end[1])){
+            buildByX();
+        }else{
+            buildByY();
+        };
         return lawyer;
-    }
+    };
 };
 
 let f = new Field(10, 10, "#");
-f.matrix = f.line([3, 3], [7, 5], "0");
+f.matrix = f.line([3, 3], [3, 7], "0");
 let str = f.print("");
 console.log(str);
