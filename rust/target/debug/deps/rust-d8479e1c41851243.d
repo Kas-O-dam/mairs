748de0e053429772fbce6e18f7102c779ce957e0
svenska kartläggare <omapper926@gmail.com>
2023-06-16 14:33:46 +0300

    Slava Bogu, i'm begin mai on rust

---
+++ rust/target/debug/deps/rust-d8479e1c41851243.d
@@ -0,0 +1,5 @@
+/home/damir/Workspace/work/maipy/rust/target/debug/deps/rust-d8479e1c41851243.rmeta: src/main.rs
+
+/home/damir/Workspace/work/maipy/rust/target/debug/deps/rust-d8479e1c41851243.d: src/main.rs
+
+src/main.rs:
