baa4fe92b2fd637d64063c206cd402160c8a5cca
svenska kartläggare <omapper926@gmail.com>
2023-06-16 16:22:16 +0300

    python can work with layers

--- python/field.py
+++ python/field.py
@@ -2,81 +2,95 @@ from tile import Tile
 from palette import Palette
 
 class Field:
-	def __init__(this,x,y,data=[],char="#"):
-		this.sequence = []
-		this.x=x+1
-		this.y=y+1
-		if data==[]:
-			this.matrix=[]
-			for i in range(0,x+1):
-				this.matrix.append([])
-				for j in range(0,y+1):
-					this.matrix[len(this.matrix)-1].append(Tile(char=char, rgba=Palette[char]))
-	def print(this, separate=""):
-		for y in range(len(this.matrix[0])-1):
-			for x in range(len(this.matrix)-1):
-				print(this.matrix[x][y].char + separate, end="")
+	def __init__(self, x, y, char="#"):
+		self.sequence = list()
+		self.x=x
+		self.y=y
+		self.sequence.append(self.build_layer())
+		self.default_char = char
+	def build_layer(self, char="#"):
+		matrix=[]
+		for i in range(0, self.x):
+			matrix.append([])
+			for j in range(0, self.y):
+				matrix[len(matrix)-1].append(Tile(char=char, rgba=Palette[char]))
+		return matrix
+	def print(self, separate=""):
+		sequence_to_print = self.sequence[0]
+		for index_x, x in enumerate(self.sequence):
+			for index_y, y in enumerate(x):
+				for tile in y:
+					match tile.char:
+						case "\n":
+							continue
+						case None:
+							continue
+						case _ :
+							sequence_to_print[index_x][index_y].char = tile.char + separate
+		for y in range(self.y):
+			for x in range(self.x):
+				print(sequence_to_print[x][y].char + separate, end="")
 			print()
-	def line(this, begin, end, char="#"):
-		lawyer = this.matrix
-		def buildByY():
+	def line(self, begin, end, char="#"):
+		layer = self.build_layer()
+		def build_by_y():
 			print("Build by y")
 			points = []
-			koefX = 0
-			cycleBegin = begin[1]
-			cycleEnd = end[1] + 1
-			beginX = begin[0]
+			koef_x = 0
+			cycle_begin = begin[1]
+			cycle_end = end[1] + 1
+			begin_x = begin[0]
 			if end[0] < begin[0]:
-				koefX = -1
+				koef_x = -1
 			else:
-				koefX = 1
+				koef_x = 1
 			if begin[1] > end[1]:
-				cycleBegin = end[1]
-				cycleEnd = begin[1] + 1
-				beginX = end[0]
-				koefX = -koefX
-			cornerKoef = abs(end[0]-begin[0])/abs(end[1]-begin[1])
+				cycle_begin = end[1]
+				cycle_end = begin[1] + 1
+				begin_x = end[0]
+				koef_x = -koef_x
+			corner_koef = abs(end[0]-begin[0])/abs(end[1]-begin[1])
 			error = 0
-			x = beginX
-			for y in range(cycleBegin, cycleEnd):
+			x = begin_x
+			for y in range(cycle_begin, cycle_end):
 				if error >= 0.5:
-					x += koefX
+					x += koef_x
 					error -= 1
 				points.append((x, y))
-				print("x: ", x, " y: ", y, " e: ", error, " ck: ", cornerKoef)
-				error += cornerKoef
+				print("x: ", x, " y: ", y, " e: ", error, " ck: ", corner_koef)
+				error += corner_koef
 			for coors in points:
-				lawyer[coors[0]][coors[1]].char = char
-		def buildByX():
+				layer[coors[0]][coors[1]].char = char
+		def build_by_x():
 			print("Build by x")
 			points = []
-			koefY = 0
-			cycleBegin = begin[0]
-			cycleEnd = end[0] + 1
-			beginY = begin[1]
+			koef_y = 0
+			cycle_begin = begin[0]
+			cycle_end = end[0] + 1
+			begin_y = begin[1]
 			if end[1] < begin[1]:
-				koefY = -1
+				koef_y = -1
 			else:
-				koefY = 1
+				koef_y = 1
 			if begin[0] > end[0]:
-				cycleBegin = end[0]
-				cycleEnd = begin[0] + 1
-				beginY = end[1]
-				koefY = -koefY
-			cornerKoef = abs((end[1]-begin[1])/(end[0]-begin[0]))
+				cycle_begin = end[0]
+				cycle_end = begin[0] + 1
+				begin_y = end[1]
+				koef_y = -koef_y
+			corner_koef = abs((end[1]-begin[1])/(end[0]-begin[0]))
 			error = 0
-			y = beginY
-			for x in range(cycleBegin, cycleEnd):
+			y = begin_y
+			for x in range(cycle_begin, cycle_end):
 				if error >= 0.5:
-					y += koefY
+					y += koef_y
 					error -= 1
 				points.append((x, y))
-				print("x: ", x, " y: ", y, " e: ", error, " ck: ", cornerKoef)
-				error += cornerKoef
+				print("x: ", x, " y: ", y, " e: ", error, " ck: ", corner_koef)
+				error += corner_koef
 			for coors in points:
-				lawyer[coors[0]][coors[1]].char = char
+				layer[coors[0]][coors[1]].char = char
 		if abs(begin[0]-end[0]) >= abs(begin[1]-end[1]):
-			buildByX()
+			build_by_x()
 		else:
-			buildByY()
-		return lawyer
+			build_by_y()
+		return layer
--- python/main.py
+++ python/main.py
@@ -2,8 +2,8 @@ from field import Field
 
 f = Field(10, 10)
 # calls for testing
-# f.matrix = f.line((5, 4), (9, 4), '0') # a
-# f.matrix = f.line((9, 4), (5, 4), '1') # a reverse
+# f.sequence[0] = f.line((5, 4), (9, 4), '0') # a
+# f.sequence.append(f.line((9, 4), (5, 4), '1')) # a reverse
 # f.matrix = f.line((5, 4), (9, 2), '0') # b
 # f.matrix = f.line((9, 2), (5, 4), '1') # b reverse
 # f.matrix = f.line((5, 4), (9, 0), '0') # c
@@ -11,4 +11,3 @@ f = Field(10, 10)
 # f.matrix = f.line((5, 4), (7, 0), '0') # d
 # f.matrix = f.line((7, 0), (5, 4), '1') # d reverse
 # f.matrix = f.line((0, 0), (49, 9), '*')
-f.print()
