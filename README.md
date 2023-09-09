a858f0f1b2d220cdeeffa407d29f946c6ce2a46e
dinskoy <omapper926@gmail.com>
2023-09-09 18:23:10 +0300

    colourize output and convert characters to struct

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Hey! This repo is a library for creating a simplest character grafic in a comman
 ```Rust
 const CRATES_IO = "https://crates.io/crates/mairs".to_string();
 
-let mut documentation = "/DOCS.md".to_string();
-let mut repo_https = "https://github.com/dinskoy/mai.git".to_string();
-let mut repo_ssh = "git@github.com:dinskoy/mai.git".to_string();
+let mut documentation = "DOCS/DOCS-0.1.5.md".to_string();
+let repo_https = "https://github.com/dinskoy/mairs.git".to_string();
+let repo_ssh = "git@github.com:dinskoy/mairs.git".to_string();
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,51 @@
-#[derive(Debug)]
+extern crate colored;
 
-//before published, SET AS PUBLIC!!!
-pub struct Layer {
+use colored::Colorize;
+
+#[derive(Debug)]
+pub struct Character {
+   pub context: char,
+   pub red: u8,
+   pub green: u8,
+   pub blue: u8,
+   pub unicode: String
+}
+pub struct Layer<'layer>{
    pub x: usize,
    pub y: usize,
-   pub context: Vec<Vec<char>>
+   pub context: Vec<Vec<&'layer Character>>
 }
-impl Layer {
-   pub fn build(&mut self, sym:char){
+impl<'a> Layer <'a> {
+   pub fn build(&mut self, sym:&'a Character){
       self.context = Vec::with_capacity(self.x);
       for _step_x in 0..self.x {
-         let empty_vec: Vec<char> = Vec::with_capacity(self.y);
+         let empty_vec: Vec<&Character> = Vec::with_capacity(self.y);
          self.context.push(empty_vec);
          for _step_y in 0..self.y {
             self.context[_step_x].push(sym);
          };
       };
    }
-   pub fn copy(&self, begin:[usize; 2], end:[usize; 2]) -> Vec<Vec<char>>{
-      let mut slice: Vec<Vec<char>> = Vec::with_capacity((begin[0] as i32 - end[0] as i32).abs() as usize);
-      for step_x in begin[0]..end[0] - 1 {
+   pub fn copy(&self, begin:[usize; 2], end:[usize; 2]) -> Vec<Vec<&Character>>{
+      let mut slice: Vec<Vec<&Character>> = Vec::with_capacity((begin[0] as i32 - end[0] as i32).abs() as usize);
+      for step_x in begin[0]..end[0]{
          slice.push(Vec::with_capacity((begin[1] as i32 - end[1] as i32).abs() as usize));
          let last_slice_row: usize = slice.len() - 1;
-         for step_y in begin[1]..end[1] - 1 {
+         for step_y in begin[1]..end[1]{
             slice[last_slice_row].push(self.context[step_x][step_y]);
          };
       };
       slice
    }
-   pub fn paste(&mut self, slice:&Vec<Vec<char>>, begin:[usize; 2]){
+   pub fn paste(&mut self, slice:&Vec<Vec<&'a Character>>, begin:[usize; 2]){
       for step_x in 0..slice.len() {
          for step_y in 0..slice[0].len() {
             self.context[begin[0] + step_y][begin[1] + step_x] = slice[step_x][step_y]; //that, what x & y did replaced have only one reason - it is work correctly...
          };
       };
    }
-   pub fn cut(&mut self, begin:[usize; 2], end:[usize; 2], sym:char) -> Vec<Vec<char>>{
-      let mut slice: Vec<Vec<char>> = Vec::new();
+   pub fn cut(&mut self, begin:[usize; 2], end:[usize; 2], sym:&'a Character) -> Vec<Vec<&Character>>{
+      let mut slice: Vec<Vec<&Character>> = Vec::new();
       for step_x in begin[0]..end[0] {
          slice.push(Vec::new());
          let last_slice_row: usize = slice.len() - 1;
@@ -48,13 +57,13 @@ impl Layer {
       slice
    }
    // GEOMETRY
-   pub fn horizontal(&mut self, mut x:[usize; 2], y:usize, sym:char){
+   pub fn horizontal(&mut self, mut x:[usize; 2], y:usize, sym:&'a Character){
       if x[0] > x[1] {(x[0], x[1]) = (x[1], x[0])};
       for step in x[0]..x[1] {
          self.context[step][y] = sym;
       };
    }
-   pub fn vertical(&mut self, x:usize, mut y:[usize; 2], sym:char){
+   pub fn vertical(&mut self, x:usize, mut y:[usize; 2], sym:&'a Character){
       if y[0] > y[1] {(y[0], y[1]) = (y[1], y[0])};
       for step in y[0]..y[1] {
          self.context[x][step] = sym;
@@ -67,7 +76,7 @@ impl Layer {
 		| |____  | |  | | \\ |  | |----'
 		|_____|  |_|  |_|  \_|  |______|
    */
-   pub fn line(&mut self, begin:[usize; 2], end:[usize; 2], sym:char){
+   pub fn line(&mut self, begin:[usize; 2], end:[usize; 2], sym:&'a Character){
       if (begin[0] as i32 - end[0] as i32).abs() >= (begin[1] as i32 - end[1] as i32).abs(){
          println!("build by x"); //debug
          let mut points: Vec<[usize; 2]> = Vec::new();
@@ -139,7 +148,7 @@ impl Layer {
       | |\ \  | ,---' \ \____     | |
       |_| \_\ |_____|  \____/     |_|
    */
-   pub fn rect(&mut self, mut begin:[usize; 2], mut end:[usize; 2], sym:char){
+   pub fn rect(&mut self, mut begin:[usize; 2], mut end:[usize; 2], sym:&'a Character){
       //if user want draw reverse rect
       if begin[0] > end[0] { (begin[0], end[0]) = (end[0], begin[0]) };
       if begin[1] > end[1] { (begin[1], end[1]) = (end[1], begin[1]) };
@@ -156,16 +165,16 @@ impl Layer {
       | |\ \ | \__/ | | \__/ | | |\\| | | |_/ /
       |_| \_\ \____/   \____/  |_| \__| |__,-'
    */
-  pub fn round(&mut self, center:[i32; 2], radius:i32, char:char){
+  pub fn round(&mut self, center:[i32; 2], radius:i32, sym:&'a Character){
    let a = [center[0], center[1] - radius];
    let b = [center[0] + radius, center[1]];
    let c = [center[0], center[1] + radius];
    let d = [center[0] - radius, center[1]];
    let mut  mover = a;
-   self.context[a[0] as usize][a[1] as usize] = char;
-   self.context[b[0] as usize][b[1] as usize] = char;
-   self.context[c[0] as usize][c[1] as usize] = char;
-   self.context[d[0] as usize][d[1] as usize] = char;
+   self.context[a[0] as usize][a[1] as usize] = sym;
+   self.context[b[0] as usize][b[1] as usize] = sym;
+   self.context[c[0] as usize][c[1] as usize] = sym;
+   self.context[d[0] as usize][d[1] as usize] = sym;
    while mover[0] != b[0] || mover[1] != b[1] {
       let b1 = [mover[0] + 1, mover[1]];
       let b2 = [mover[0], mover[1] + 1];
@@ -189,7 +198,7 @@ impl Layer {
       if min == hipotenuse2 { mover = b2 };
       if min == hipotenuse3 { mover = b3 };
       //console.debug(mover, list); //
-      self.context[mover[0] as usize][mover[1] as usize] = char;
+      self.context[mover[0] as usize][mover[1] as usize] = sym;
    };
    while mover[0] != c[0] || mover[1] != c[1] {
       let c1 = [mover[0] - 1, mover[1]];
@@ -214,7 +223,7 @@ impl Layer {
       if min == hipotenuse2 { mover = c2 };
       if min == hipotenuse3 { mover = c3 };
       //console.debug(mover, list); //
-      self.context[mover[0] as usize][mover[1] as usize] = char;
+      self.context[mover[0] as usize][mover[1] as usize] = sym;
    };
    while mover[0] != d[0] || mover[1] != d[1] {
        let d1 = [mover[0] - 1, mover[1]];
@@ -239,7 +248,7 @@ impl Layer {
        if min == hipotenuse2 { mover = d2 };
        if min == hipotenuse3 { mover = d3 };
        //console.debug(mover, list); //
-       self.context[mover[0] as usize][mover[1] as usize] = char;
+       self.context[mover[0] as usize][mover[1] as usize] = sym;
    };
    while mover[0] != a[0] || mover[1] != a[1] {
        let a1 = [mover[0] + 1, mover[1]];
@@ -264,40 +273,37 @@ impl Layer {
        if min == hipotenuse2 { mover = a2 };
        if min == hipotenuse3 { mover = a3 };
        //console.debug(mover, list); //
-       self.context[mover[0] as usize][mover[1] as usize] = char;
+       self.context[mover[0] as usize][mover[1] as usize] = sym;
    };
 }
 }
 pub struct Field<'a>{
-   pub seq: Vec<&'a mut Layer>,
+   pub seq: Vec<&'a mut Layer<'a>>,
    pub default_char: char,
 }
 
 impl Field<'_>{
-   pub fn unite(&self, layers:&Vec<&mut Layer>) -> Vec<Vec<char>>{
-      //println!("{:?}", layers);
-      let mut layer_result: Vec<Vec<char>> = layers[0].copy([0, 0], [layers[0].x, layers[0].y]);
+   pub fn unite<'a>(&'a self, layers:&'a Vec<&mut Layer>) -> Vec<Vec<&Character>>{
+      let mut layer_result: Vec<Vec<&Character>> = layers[0].copy([0, 0], [layers[0].x, layers[0].y]);
       //println!("{:?}", layer_result);
       for layer in 1..layers.len() {
          for x in 0..layers[layer].context.len() { 
             for tile in 0..layers[layer].context[x].len() {
-               if layers[layer].context[x][tile] != self.default_char {
+               if layers[layer].context[x][tile].context != self.default_char {
                   layer_result[x][tile] = layers[layer].context[x][tile];
                };
                //println!("l: {}, x: {}, y: {}, c: {}, is space: {}", x, y, tile, layers[x][y][tile], layers[x][y][tile] == self.default_char);
             };
          };
       };
-      //println!("{:?}", layer_result);
       layer_result
    }
    pub fn print(&self){
       let all_layers = self.unite(&self.seq);
-
-      for y in 0..all_layers.len() - 1 {
+      for y in 0..all_layers.len()-1 {
          println!("");
-         for x in 0..all_layers[0].len() - 1 {
-            print!("{}", all_layers[x][y]);
+         for x in 0..all_layers[0].len()-1 {
+            print!("{}", all_layers[x][y].context.to_string().truecolor(all_layers[x][y].red, all_layers[x][y].green, all_layers[x][y].blue));
          };
       };
    }
@@ -307,13 +313,58 @@ impl Field<'_>{
       for y in 0..all_layers.len() - 1 {
          println!("");
          for x in 0..all_layers[0].len() - 1 {
-            print!("{}{}", all_layers[x][y], sepchar);
+            print!("{}{}", all_layers[x][y].context, sepchar);
          };
       };
    }
    
 }
+
 fn main(){
-   //const X: usize = 40;
-   //const Y: usize = 20;
+   let aa = Character{context: '<', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ab = Character{context: '(', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ac = Character{context: '°', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ad = Character{context: ' ', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ae = Character{context: ')', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ag = Character{context: '/', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ah = Character{context: '=', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let ai = Character{context: '|', red: 0, green: 255, blue: 0, unicode: "".to_string()};
+   let land_char: Character = Character {context: '*', red: 192, green: 128, blue: 32, unicode: "".to_string()};
+   let air_char: Character = Character {context: ' ', red: 0, green: 0, blue: 0, unicode: "".to_string()};
+   let dino: Vec<Vec<&Character>> = vec![
+      vec![
+         &aa,
+         &ab,
+         &ac,
+         &ad,
+         &ae
+      ],
+      vec![
+         &aa,
+         &ag,
+         &ad,
+         &ah,
+         &ad
+      ],
+      vec![
+         &ag,
+         &ai,
+         &ai,
+         &ad,
+         &ad
+      ],
+   ];
+   
+   let mut field: Field = Field {seq: Vec::new(), default_char: ' '};
+   let mut main_layer: Layer = Layer {x: 32, y: 16, context: Vec::new()};
+   main_layer.build(&air_char);   
+   main_layer.horizontal([0, 32], 15, &land_char);
+   
+   let mut dino_layer: Layer = Layer {x: 32, y: 16, context: Vec::new()};
+   dino_layer.build(&air_char);
+   dino_layer.paste(&dino, [0, 12]);
+   
+   field.seq.push(&mut main_layer);
+   field.seq.push(&mut dino_layer);
+   field.print()
 }
